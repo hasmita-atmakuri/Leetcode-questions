d151538c783b7a75416d0981e761e6ee6ee7593d
hasmita-atmakuri <153909891+hasmita-atmakuri@users.noreply.github.com>
2025-03-15 12:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.56%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -9,20 +9,20 @@ public:
         int endCol = n-1;
         vector<int> ans;
         while(stCol<=endCol && stRow<=endRow){
-            //top
+
             for(int j=stCol;j<=endCol;j++){
                 ans.push_back(matrix[stRow][j]);
             }
-            //right
+
             for(int i=stRow+1;i<=endRow;i++){
                 ans.push_back(matrix[i][endCol]);
             }
-            //bottom
+
             for(int j=endCol-1;j>=stCol;j--){
                 if(stRow==endRow) break;
                 ans.push_back(matrix[endRow][j]);
             }
-            //left
+
             for(int i=endRow-1;i>stCol;i--){
                 if(stCol==endCol) break;
                 ans.push_back(matrix[i][stCol]);
